2dd907250cf218e54a811dca74b309b7074153bb
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-03 12:08:56 -0500

    useEffect with useCallback

--- src/FetchCard.js
+++ src/FetchCard.js
@@ -1,29 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import getPosts from './helpers/getPosts';
 import getUser from './helpers/getUser';
 
-const initialUser = {
-	name: "",
-	email: ""
-}
+// const initialUser = {
+// 	name: "",
+// 	email: ""
+// }
+
+// const initialPosts = [
+// 	{ id: 1, title: "Post 1"},
+// 	{ id: 2, title: "Post 2"},
+// ]
 
 const FetchCard = () => {
-	const [user, setUser] = useState(initialUser)
+	const [ user, setUser ] = useState({})
+	const [ posts, setPosts  ] = useState([])
 	
 	const updateUser = () => {
 		getUser()
 			.then((newUser) => {
-				setUser(newUser)  // ==> Lo datos que obtendremos lo llevaremos a esta funcion. 
+				setUser(newUser);  // ==> Lo datos que obtendremos lo llevaremos a esta funcion. 
 			})
 	}
 
+	const updatePosts = useCallback(() => { 
+		getPosts(user.id)
+			.then((newPosts) => {
+				setPosts(newPosts)
+			})
+	}, [user.id]) // Lo actualizae cuando cambie el id
+	
 	useEffect(() => {
 		updateUser();
-	},[])
+	}, [])
+
+	useEffect(() => {
+		if (user?.id) {
+			updatePosts();
+		}
+		// Cada que exista un cambi en user se ejecutara esta funcion.
+	}, [user], updatePosts)
 
   return (
 	  <div>
+		  <button onClick={updateUser}>
+			  Another User
+		  </button>
 		  <h1>{user.name}</h1>
 		  <h2>{user.email}</h2>
+		  <br/>
+
+		  <h2>Posts - user: {user.id}</h2>
+		  <ul>
+			  {posts.map(post => (
+				  <li key={post.id}>{post.name}</li>
+			  ))}
+		  </ul>
 	  </div>
   )
 }
--- src/helpers/getUser.js
+++ src/helpers/getUser.js
@@ -1,8 +1,6 @@
 const getUser = async () => {
-	const userId = Math.random() * 10; // Generar un nuemero alatorio entre 0 y 1.
-	console.log(userId);
-
-	const url = 'https://jsonplaceholder.typicode.com/users/1';
+	const userId = Math.floor(Math.random() * 10) + 1; // Generar un numero entero 
+	const url = `https://jsonplaceholder.typicode.com/users/${userId}`;
 	const res = await fetch(url);
 	const user = await res.json();
 
